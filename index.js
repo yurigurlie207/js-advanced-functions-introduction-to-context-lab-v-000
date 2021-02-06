f6c73f58f667d48efae64049844021f4e877108a
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-06 14:57:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let createTimeInEvent = function(employee, dateStamp){
     employee.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
-        date: date,
+        date: date
     })
 
     return employee
