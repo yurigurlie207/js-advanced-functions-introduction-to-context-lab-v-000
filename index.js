fd62fc41e8bbc2cda57ed441cfd145db9268e01c
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-06 14:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Your code here
 let createEmployeeRecord = function(row){
   return {
-    firstName: row[0]
+    firstName: row[0],
+    familyName: row[1],
+      title: row[2],
+      payPerHour: row[3],
   }
 
 }
