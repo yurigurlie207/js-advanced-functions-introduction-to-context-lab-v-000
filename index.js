dc1539d9c42cfffd6ee9e4c0fffdf298b62f56b2
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-06 14:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ let createEmployeeRecords = function(employeeRowData) {
         return createEmployeeRecord(row)
     })
 }
+
+let createTimeInEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
